176d5d793832736bab24af15baf6ac7f54806b31
Sabin Pandey <pandeyfcb7849@gmail.com>
2024-08-20 16:58:25 +0545

    create type with respect to table fields and it's type

--- src/interface.ts
+++ src/interface.ts
@@ -1,13 +1,29 @@
 export type ObjectValues<T> = T[keyof T];
 
-export type TChapter = { title: string; id: number };
-
-export type TLesson = { title: string; chapter_id: TChapter["id"] };
-
 export type TTableInfo = {
   tableName: string;
   fieldsWithType: {
     fieldName: string;
     dataType: string;
   }[];
+};
+
+export type TChapterWithoutNoOfLessons = {
+  title: string;
+  chapter_id: number;
+};
+
+export type TLessonFieldsWitoutId = {
+  title: string;
+  chapter_id: TChapterFields["chapter_id"];
+};
+
+// 'chapters' table column fields & it's type.
+export type TChapterFields = TChapterWithoutNoOfLessons & {
+  no_of_lessons: number;
+};
+
+// 'lessons' table column fields & it's type
+export type TLessonFields = TLessonFieldsWitoutId & {
+  lesson_id: number;
 };
