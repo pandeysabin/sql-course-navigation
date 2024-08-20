eea03eb83d19f8c9e57fc8470d46055405792027
Sabin Pandey <pandeyfcb7849@gmail.com>
2024-08-20 17:01:05 +0545

    use table types properly with adding table data

--- src/consts.ts
+++ src/consts.ts
@@ -1,4 +1,10 @@
-import { ObjectValues, TChapter, TLesson } from "./interface";
+import {
+  ObjectValues,
+  TChapterFields,
+  TChapterWithoutNoOfLessons,
+  TLessonFields,
+  TLessonFieldsWitoutId,
+} from "./interface";
 
 export const TABLES = {
   CHAPTERS: "chapters",
@@ -7,97 +13,65 @@ export const TABLES = {
 
 export type TTable = ObjectValues<typeof TABLES>;
 
-const CHAPTER_TABLE_FIELDS = ["chapter_id", "title", "no_of_lessons"] as const;
-export type TChapterTableField = (typeof CHAPTER_TABLE_FIELDS)[number];
+export const CHAPTERS_TABLE_FIELDS_WITH_DATA_TYPE: Record<
+  keyof TChapterFields,
+  string
+> = {
+  chapter_id: "INT",
+  title: "varchar(50)",
+  no_of_lessons: "INT",
+};
 
-export const CHAPTER_FIELDS_WITH_DATA_TYPE: Record<TChapterTableField, string> =
-  {
-    chapter_id: "INT",
-    title: "varchar(50)",
-    no_of_lessons: "INT",
-  };
+const CHAPTERS_TABLE_FIELDS = Object.keys(
+  CHAPTERS_TABLE_FIELDS_WITH_DATA_TYPE
+) as unknown as (keyof TChapterFields)[];
 
-const LESSON_TABLE_FIELDS = [
-  "lesson_id",
-  "title",
-  "chapter_id",
-  // "link",
-  // "difficulty",
-] as const;
-export type TLessonTableField = (typeof LESSON_TABLE_FIELDS)[number];
-
-export const LESSONS_FIELDS_DATA_TYPE: Record<TLessonTableField, string> = {
-  lesson_id: "int",
+export const LESSONS_TABLE_FIELDS_DATA_TYPE: Record<
+  keyof TLessonFields,
+  string
+> = {
+  lesson_id: "INT",
   title: "varchar(50)",
   chapter_id: "varchar(50)",
-  // difficulty: "varchar(50)",
-  // link: "varchar(50)",
 } as const;
 
-export const FIELDS_BY_TABLE = Object.values(TABLES).reduce(
-  (
-    acc: Record<
-      TTable,
-      readonly TChapterTableField[] | readonly TLessonTableField[]
-    >,
-    curr
-  ) => {
-    switch (curr) {
-      case "chapters":
-        acc = {
-          ...acc,
-          [curr]: CHAPTER_TABLE_FIELDS,
-        };
-        break;
+const LESSONS_TABLE_FIELDS = Object.keys(
+  LESSONS_TABLE_FIELDS_DATA_TYPE
+) as unknown as (keyof TLessonFields)[];
 
-      case "lessons":
-        acc = { ...acc, [curr]: LESSON_TABLE_FIELDS };
-        break;
-
-      default:
-        return acc;
-    }
-
-    return acc;
-  },
-
-  { lessons: [], chapters: [] }
-);
-
-export const CHAPTERS: TChapter[] = [
+export const CHAPTERS: TChapterWithoutNoOfLessons[] = [
   {
-    id: 1,
+    chapter_id: 1,
     title: "Introduction",
   },
   {
-    id: 2,
+    chapter_id: 2,
     title: "Select Records",
   },
   {
-    id: 3,
+    chapter_id: 3,
     title: "Aggregrate Functions",
   },
   {
-    id: 4,
+    chapter_id: 4,
     title: "Filter Records",
   },
-
   {
-    id: 5,
+    chapter_id: 5,
     title: "JOINs",
   },
-  { id: 6, title: "Subquery" },
-  { id: 7, title: "Insert, Update & Delete" },
-  { id: 8, title: "Working With Tables" },
-  { id: 9, title: "Constraints" },
+  { chapter_id: 6, title: "Subquery" },
+  { chapter_id: 7, title: "Insert, Update & Delete" },
+  { chapter_id: 8, title: "Working With Tables" },
+  { chapter_id: 9, title: "Constraints" },
   {
-    id: 10,
+    chapter_id: 10,
     title: "Additional Topics",
   },
-  { id: 11, title: "SQL Queries" },
+  { chapter_id: 11, title: "SQL Queries" },
 ];
 
-const LESSONS: TLesson[] = [
+const LESSONS: TLessonFieldsWitoutId[] = [
   {
     title: "Getting Started",
     chapter_id: 1,
@@ -157,17 +131,19 @@ const LESSONS: TLesson[] = [
   { chapter_id: 11, title: "What's Next?" },
 ];
 
-export const LESSONS_TABLE_DATA: Record<TLessonTableField, string | number>[] =
-  LESSONS.map(({ chapter_id, title }, lessonIdx) => ({
-    chapter_id,
-    lesson_id: lessonIdx + 1,
-    title,
-  }));
+export const LESSONS_TABLE_DATA: Record<
+  keyof TLessonFields,
+  string | number
+>[] = LESSONS.map(({ chapter_id, title }, lessonIdx) => ({
+  chapter_id,
+  lesson_id: lessonIdx + 1,
+  title,
+}));
 
 export const CHAPTERS_TABLE_DATA: Record<
-  TChapterTableField,
+  keyof TChapterFields,
   string | number
->[] = CHAPTERS.map(({ id, title: name }) => ({
+>[] = CHAPTERS.map(({ chapter_id: id, title: name }) => ({
   chapter_id: id,
   title: name,
   no_of_lessons: LESSONS_TABLE_DATA.filter(
@@ -175,37 +151,34 @@ export const CHAPTERS_TABLE_DATA: Record<
   ).length,
 }));
 
-export const CREATE_LESSON_TABLE_QUERY = `CREATE TABLE ${TABLES.LESSONS} (
-  ${Object.keys(LESSONS_FIELDS_DATA_TYPE)
-    .map(
-      (fieldWithDataType) =>
-        // @ts-expect-error Object.keys() doesn't give constant value
-        `${fieldWithDataType} ${LESSONS_FIELDS_DATA_TYPE[fieldWithDataType]}`
-    )
-    .join(", ")}
+export const CREATE_LESSONS_TABLE_QUERY = `CREATE TABLE ${TABLES.LESSONS} (
+  ${LESSONS_TABLE_FIELDS.map(
+    (fieldWithDataType) =>
+      `${fieldWithDataType} ${LESSONS_TABLE_FIELDS_DATA_TYPE[fieldWithDataType]}`
+  ).join(", ")}
   );` as const;
 
-export const DATA_TO_INSERT_TO_LESSON_TABLE_QUERY = `INSERT INTO ${
+export const DATA_TO_INSERT_TO_LESSONS_TABLE_QUERY = `INSERT INTO ${
   TABLES.LESSONS
-} (${LESSON_TABLE_FIELDS.map((field) => field).join(", ")})
-  VALUES
-  ${LESSONS_TABLE_DATA.map(({ chapter_id, lesson_id, title }) => {
-    return `("${lesson_id}", "${title}", "${chapter_id}")`;
-  }).join(",")};` as const;
+} (
+lesson_id, title, chapter_id
+)
+VALUES
+${LESSONS_TABLE_DATA.map(({ chapter_id, lesson_id, title }) => {
+  return `("${lesson_id}", "${title}", "${chapter_id}")`;
+}).join(",")};` as const;
 
 export const CREATE_CHAPTERS_TABLE_QUERY = `CREATE TABLE ${TABLES.CHAPTERS} (
-${Object.keys(CHAPTER_FIELDS_WITH_DATA_TYPE)
-  .map(
-    // @ts-expect-error Object.keys() is unable to find the type of the parameter
-    (fieldName) => `${fieldName} ${CHAPTER_FIELDS_WITH_DATA_TYPE[fieldName]}`
-  )
-  .join(", ")}
+${CHAPTERS_TABLE_FIELDS.map(
+  (fieldName) =>
+    `${fieldName} ${CHAPTERS_TABLE_FIELDS_WITH_DATA_TYPE[fieldName]}`
+).join(", ")}
 );` as const;
 
-export const DATA_TO_INSERT_TO_CHAPTER_TABLE_QUERY = `INSERT INTO ${
+export const DATA_TO_INSERT_TO_CHAPTERS_TABLE_QUERY = `INSERT INTO ${
   TABLES.CHAPTERS
 } (
-${CHAPTER_TABLE_FIELDS.map((field) => field).join(", ")}
+chapter_id, title, no_of_lessons
 ) VALUES ${CHAPTERS_TABLE_DATA.map(
   ({ chapter_id, title, no_of_lessons }) =>
     `("${chapter_id}", "${title}", "${no_of_lessons}")`
